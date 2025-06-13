7eb4f1b3ab44ada529125e6a9721a04635f651a0
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-13 11:12:10 +0530

    Added solution for odd even numbers-Day3

--- Java_Control_Flow/src/OddEvenNumbers.java
+++ Java_Control_Flow/src/OddEvenNumbers.java
@@ -1,14 +1,12 @@
-package Level_1;
-
 import java.util.Scanner;
 
 public class OddEvenNumbers {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a number: ");
+        System.out.print("Enter a natural number: ");
         int n = sc.nextInt();
         if (n < 1) {
-            System.out.println("Not a natural number");
+            System.out.println("Not a natural number!");
             return;
         }
         for (int i = 1; i <= n; i++) {
