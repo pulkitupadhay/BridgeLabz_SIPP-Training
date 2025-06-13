b670c1ff5db839315e78d354a84fe03815f3494f
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-13 10:58:14 +0530

    Added solution for harshed Number-Day3

---
+++ Java_Control_Flow/src/HarshadNumber.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HarshadNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt(), sum = 0, temp = num;
+        while (temp > 0) {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        System.out.println(num + (num % sum == 0 ? " is a Harshad number." : " is not a Harshad number."));
+    }
+}
