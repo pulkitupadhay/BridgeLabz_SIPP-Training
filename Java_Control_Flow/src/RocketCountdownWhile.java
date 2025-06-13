bafe2bf26cbd7f832612fea7cd84f34cf614012b
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-13 10:33:42 +0530

    Added solution for Rocket Count Down by while loop-Day3

---
+++ Java_Control_Flow/src/RocketCountdownWhile.java
@@ -0,0 +1,15 @@
+package Level_1;
+
+import java.util.Scanner;
+
+public class RocketCountdownWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter countdown start: ");
+        int counter = sc.nextInt();
+        while (counter >= 1) {
+            System.out.println(counter);
+            counter--;
+        }
+    }
+}
