8a01e04b5e70d746d0bbca6a98fa36e51cf1f3aa
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-17 10:17:55 +0530

    Added number checker utility (perfect, abundant, deficient, strong)

---
+++ Java_Methods/src/NumberChecker.java
@@ -0,0 +1,45 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class NumberChecker {
+    public static boolean isPerfect(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) if (n % i == 0) sum += i;
+        return sum == n;
+    }
+
+    public static boolean isAbundant(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) if (n % i == 0) sum += i;
+        return sum > n;
+    }
+
+    public static boolean isDeficient(int n) {
+        int sum = 0;
+        for (int i = 1; i < n; i++) if (n % i == 0) sum += i;
+        return sum < n;
+    }
+
+    public static boolean isStrong(int n) {
+        int sum = 0, temp = n;
+        while (temp > 0) {
+            int d = temp % 10;
+            int fact = 1;
+            for (int i = 1; i <= d; i++) fact *= i;
+            sum += fact;
+            temp /= 10;
+        }
+        return sum == n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Perfect? " + isPerfect(n));
+        System.out.println("Abundant? " + isAbundant(n));
+        System.out.println("Deficient? " + isDeficient(n));
+        System.out.println("Strong? " + isStrong(n));
+    }
+}
