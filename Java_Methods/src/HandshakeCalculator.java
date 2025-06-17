8bb2889cbadf2cbbe8d6f160ed7ba1514a24c811
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-17 10:10:49 +0530

    Add program to calculate maximum handshakes among students

---
+++ Java_Methods/src/HandshakeCalculator.java
@@ -0,0 +1,16 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class HandshakeCalculator {
+    public static int maxHandshakes(int n) {
+        return (n * (n - 1)) / 2;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of students: ");
+        int n = sc.nextInt();
+        System.out.println("Maximum handshakes: " + maxHandshakes(n));
+    }
+}
