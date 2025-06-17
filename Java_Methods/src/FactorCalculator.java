92c598f1391813821d514ff6b101cd62b5329d61
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-17 09:59:12 +0530

    Add program to find factors and perform sum, product, and sum of squares

---
+++ Java_Methods/src/FactorCalculator.java
@@ -0,0 +1,45 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class FactorCalculator {
+    public static int[] getFactors(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; i++) if (n % i == 0) count++;
+        int[] factors = new int[count];
+        int idx = 0;
+        for (int i = 1; i <= n; i++) if (n % i == 0) factors[idx++] = i;
+        return factors;
+    }
+
+    public static int sumOfFactors(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f;
+        return sum;
+    }
+
+    public static int productOfFactors(int[] factors) {
+        int prod = 1;
+        for (int f : factors) prod *= f;
+        return prod;
+    }
+
+    public static int sumOfSquares(int[] factors) {
+        int sum = 0;
+        for (int f : factors) sum += f * f;
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int[] factors = getFactors(n);
+        System.out.print("Factors: ");
+        for (int f : factors) System.out.print(f + " ");
+        System.out.println();
+        System.out.println("Sum of factors: " + sumOfFactors(factors));
+        System.out.println("Product of factors: " + productOfFactors(factors));
+        System.out.println("Sum of squares of factors: " + sumOfSquares(factors));
+    }
+}
