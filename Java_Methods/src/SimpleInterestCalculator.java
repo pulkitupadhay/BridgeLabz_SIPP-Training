c8a706ce9937cf7d9b545c01278fa1221e32929c
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-17 10:26:08 +0530

    Added program to compute simple interest

---
+++ Java_Methods/src/SimpleInterestCalculator.java
@@ -0,0 +1,21 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class SimpleInterestCalculator {
+    public static double calculateSimpleInterest(double principal, double rate, double time) {
+        return (principal * rate * time) / 100;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter principal: ");
+        double principal = sc.nextDouble();
+        System.out.print("Enter rate: ");
+        double rate = sc.nextDouble();
+        System.out.print("Enter time: ");
+        double time = sc.nextDouble();
+        double si = calculateSimpleInterest(principal, rate, time);
+        System.out.printf("The Simple Interest is %.2f for Principal %.2f, Rate of Interest %.2f and Time %.2f\n", si, principal, rate, time);
+    }
+}
