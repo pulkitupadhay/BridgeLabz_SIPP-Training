47062b6d1f8c31282ad410f1e15f749485a6287d
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-17 08:40:50 +0530

    Added solution for Factor Finder

---
+++ Arrays_Practice_Problems/Arrays_Practice_Problems/FactorFinder.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class FactorFinder {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        int maxFactor = 10;
+        int[] factors = new int[maxFactor];
+        int index = 0;
+        for (int i = 1; i <= number; i++) {
+            if (number % i == 0) {
+                if (index == maxFactor) {
+                    maxFactor = maxFactor * 2;
+                    int[] temp = new int[maxFactor];
+                    for (int j = 0; j < index; j++) {
+                        temp[j] = factors[j];
+                    }
+                    factors = temp;
+                }
+                factors[index] = i;
+                index++;
+            }
+        }
+        for (int i = 0; i < index; i++) {
+            System.out.print(factors[i] + " ");
+        }
+        System.out.println();
+    }
+}
+
