3d95787e23dc524f2251322e3dbf8e0992a931e5
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-17 10:18:43 +0530

    Added number checker utility (prime, neon, spy, automorphic, buzz)

---
+++ Java_Methods/src/NumberChecker2.java
@@ -0,0 +1,53 @@
+package Methods;
+
+import java.util.Scanner;
+
+public class NumberChecker2 {
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) if (n % i == 0) return false;
+        return true;
+    }
+
+    public static boolean isNeon(int n) {
+        int sq = n * n, sum = 0;
+        while (sq > 0) {
+            sum += sq % 10;
+            sq /= 10;
+        }
+        return sum == n;
+    }
+
+    public static boolean isSpy(int n) {
+        int sum = 0, prod = 1, temp = n;
+        while (temp > 0) {
+            int d = temp % 10;
+            sum += d;
+            prod *= d;
+            temp /= 10;
+        }
+        return sum == prod;
+    }
+
+    public static boolean isAutomorphic(int n) {
+        int sq = n * n;
+        String nStr = String.valueOf(n);
+        String sqStr = String.valueOf(sq);
+        return sqStr.endsWith(nStr);
+    }
+
+    public static boolean isBuzz(int n) {
+        return n % 7 == 0 || n % 10 == 7;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Prime? " + isPrime(n));
+        System.out.println("Neon? " + isNeon(n));
+        System.out.println("Spy? " + isSpy(n));
+        System.out.println("Automorphic? " + isAutomorphic(n));
+        System.out.println("Buzz? " + isBuzz(n));
+    }
+}
