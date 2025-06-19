9747ff4d8a0539ba68c7e844c075a7b6d4024c8a
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-06-19 11:38:55 +0530

    Add Fibonacci sequence generator with user-defined function

---
+++ BuiltIn_Funtions/src/Fibonacci.java
@@ -0,0 +1,23 @@
+package Built_In;
+
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void printFibonacci(int n) {
+        int a = 0, b = 1;
+        System.out.print("Fibonacci: " + a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            int next = a + b;
+            System.out.print(next + " ");
+            a = b;
+            b = next;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of terms: ");
+        int n = sc.nextInt();
+        printFibonacci(n);
+    }
+}
