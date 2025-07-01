e61fde83450a4533a6e9edc65ebbc26d2099fd61
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-07-01 10:14:41 +0530

    Built school system with hierarchical inheritance: Teacher, Student, Staff from Person

---
+++ Java_Inheritence/src/Inheritence/SchoolTest.java
@@ -0,0 +1,58 @@
+package Inheritence;
+
+class Person {
+    String name;
+    int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+class Teacher extends Person {
+    String subject;
+
+    Teacher(String name, int age, String subject) {
+        super(name, age);
+        this.subject = subject;
+    }
+
+    void displayRole() {
+        System.out.println(name + " is a Teacher of " + subject);
+    }
+}
+
+class Student extends Person {
+    int grade;
+
+    Student(String name, int age, int grade) {
+        super(name, age);
+        this.grade = grade;
+    }
+
+    void displayRole() {
+        System.out.println(name + " is a Student in grade " + grade);
+    }
+}
+
+class Staff extends Person {
+    String department;
+
+    Staff(String name, int age, String department) {
+        super(name, age);
+        this.department = department;
+    }
+
+    void displayRole() {
+        System.out.println(name + " is Staff in " + department + " department");
+    }
+}
+
+public class SchoolTest {
+    public static void main(String[] args) {
+        new Teacher("Mr. Rao", 40, "Physics").displayRole();
+        new Student("Aarav", 15, 10).displayRole();
+        new Staff("Sunita", 35, "Admin").displayRole();
+    }
+}
