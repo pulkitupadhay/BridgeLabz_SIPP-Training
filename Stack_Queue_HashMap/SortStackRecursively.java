8b41dfa439a79feff9ffca1b78a5066530f28b7e
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-07-09 09:04:08 +0530

    Add recursive solution to sort a stack without using extra data structures

---
+++ Stack_Queue_HashMap/SortStackRecursively.java
@@ -0,0 +1,36 @@
+package DSA.Stack_Queue_HashMap;
+
+import java.util.Stack;
+
+public class SortStackRecursively {
+    public static void sortStack(Stack<Integer> stack) {
+        if (!stack.isEmpty()) {
+            int temp = stack.pop();
+            sortStack(stack);
+            insertSorted(stack, temp);
+        }
+    }
+
+    private static void insertSorted(Stack<Integer> stack, int element) {
+        if (stack.isEmpty() || stack.peek() <= element) {
+            stack.push(element);
+        } else {
+            int temp = stack.pop();
+            insertSorted(stack, element);
+            stack.push(temp);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(3);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+
+        System.out.println("Original Stack: " + stack);
+        sortStack(stack);
+        System.out.println("Sorted Stack: " + stack);
+    }
+}
+
