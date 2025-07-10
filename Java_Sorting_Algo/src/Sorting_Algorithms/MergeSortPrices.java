bda648f387bc9ee0b7bd3eb4b50185afe0f7d739
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-07-10 08:58:32 +0530

    Implemented Merge Sort for sorting book prices.

---
+++ Java_Sorting_Algo/src/Sorting_Algorithms/MergeSortPrices.java
@@ -0,0 +1,28 @@
+package Sorting_Algorithms;
+
+public class MergeSortPrices {
+    public static void main(String[] args) {
+        int[] prices = {499, 299, 799, 199, 399};
+        mergeSort(prices, 0, prices.length - 1);
+        for (int p : prices) System.out.print(p + " ");
+    }
+
+    public static void mergeSort(int[] arr, int l, int r) {
+        if (l < r) {
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+            merge(arr, l, m, r);
+        }
+    }
+
+    public static void merge(int[] arr, int l, int m, int r) {
+        int[] left = java.util.Arrays.copyOfRange(arr, l, m + 1);
+        int[] right = java.util.Arrays.copyOfRange(arr, m + 1, r + 1);
+        int i = 0, j = 0, k = l;
+        while (i < left.length && j < right.length)
+            arr[k++] = (left[i] <= right[j]) ? left[i++] : right[j++];
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+}
