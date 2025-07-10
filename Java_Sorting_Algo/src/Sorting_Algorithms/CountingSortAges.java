5ffe15aaeffd808e46cef1ef39c8ea376a6b5984
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-07-10 08:50:56 +0530

    Implemented Bubble Sort to arrange student marks in ascending order.

---
+++ Java_Sorting_Algo/src/Sorting_Algorithms/CountingSortAges.java
@@ -0,0 +1,17 @@
+package Sorting_Algorithms;
+
+public class CountingSortAges {
+    public static void main(String[] args) {
+        int[] ages = {15, 17, 13, 12, 15, 16, 14, 18, 10, 11};
+        countingSort(ages, 10, 18);
+        for (int age : ages) System.out.print(age + " ");
+    }
+
+    public static void countingSort(int[] arr, int min, int max) {
+        int[] count = new int[max - min + 1];
+        for (int num : arr) count[num - min]++;
+        int index = 0;
+        for (int i = 0; i < count.length; i++)
+            while (count[i]-- > 0) arr[index++] = i + min;
+    }
+}
