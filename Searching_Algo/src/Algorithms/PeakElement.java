3238111ef61d010d950247d55b5ac3d65ccf8449
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-07-11 09:01:13 +0530

    Used binary search to find peak element in an array

---
+++ Searching_Algo/src/Algorithms/PeakElement.java
@@ -0,0 +1,18 @@
+package Algorithms;
+
+public class PeakElement {
+    public static int findPeak(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (arr[mid] < arr[mid + 1]) left = mid + 1;
+            else right = mid;
+        }
+        return arr[left]; 
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 3, 8, 12, 4, 2};
+        System.out.println(findPeak(arr));
+    }
+}
