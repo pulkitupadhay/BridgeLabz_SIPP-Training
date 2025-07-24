39faa8bbb720cff62a12a60eb2314cd6538bfc52
Pulkit Upadhyay <pulkit.upadhyay9759@gmail.com>
2025-07-24 08:47:01 +0530

    Implemented Smart Warehouse System using generics with bounded types and wildcards

---
+++ Java_Generics/src/generics/Storage.java
@@ -0,0 +1,42 @@
+package generics;
+import java.util.*;
+//Abstract class for items
+abstract class WarehouseItem {
+ String name;
+ public WarehouseItem(String name) { this.name = name; }
+ public abstract void display();
+}
+
+class Electronics extends WarehouseItem {
+ public Electronics(String name) { super(name); }
+ public void display() { System.out.println("Electronics: " + name); }
+}
+
+class Groceries extends WarehouseItem {
+ public Groceries(String name) { super(name); }
+ public void display() { System.out.println("Groceries: " + name); }
+}
+
+class Furniture extends WarehouseItem {
+ public Furniture(String name) { super(name); }
+ public void display() { System.out.println("Furniture: " + name); }
+}
+
+//Generic Storage class
+class Storage<T extends WarehouseItem> {
+ private List<T> items = new ArrayList<>();
+
+ public void addItem(T item) {
+     items.add(item);
+ }
+
+ public List<T> getItems() {
+     return items;
+ }
+ 
+ public static void displayItems(List<? extends WarehouseItem> items) {
+     for (WarehouseItem item : items) {
+         item.display();
+     }
+ }
+}
