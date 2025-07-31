fa2dae6534f5b64788d3cca2e2c2bb530575fe55
Pulkit Upadhyay <pulkit.upadhyay9759@gmial.com>
2025-07-30 21:00:56 -0700

    Converted text from uppercase to lowercase and wrote it to a new file using BufferedReader and BufferedWriter

---
+++ Java_Streams/src/stream/UpperToLower.java
@@ -0,0 +1,20 @@
+package stream;
+
+import java.io.*;
+
+public class UpperToLower {
+    public static void main(String[] args) {
+        try (BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+             BufferedWriter bw = new BufferedWriter(new FileWriter("lowercase.txt"))) {
+
+            String line;
+            while ((line = br.readLine()) != null) {
+                bw.write(line.toLowerCase());
+                bw.newLine();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
