8bc6c31c14357c8c75dd4633a7135c37d8177c06
Pulkit Upadhyay <pulkit.upadhyay9759@gmial.com>
2025-07-30 20:59:42 -0700

    Used DataOutputStream and DataInputStream to write and read structured student data (int, String, double) to and from a file

---
+++ Java_Streams/src/stream/StudentDataIO.java
@@ -0,0 +1,24 @@
+package stream;
+
+import java.io.*;
+
+public class StudentDataIO {
+    public static void main(String[] args) {
+        try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("students.dat"))) {
+            dos.writeInt(1);
+            dos.writeUTF("John");
+            dos.writeDouble(3.5);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (DataInputStream dis = new DataInputStream(new FileInputStream("students.dat"))) {
+            int roll = dis.readInt();
+            String name = dis.readUTF();
+            double gpa = dis.readDouble();
+            System.out.println(roll + " " + name + " " + gpa);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
