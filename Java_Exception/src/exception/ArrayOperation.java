f7e57086155ec205e272a1656bf153419c079ca1
Pulkit Upadhyay <pulkit.upadhyay9759@gmial.com>
2025-08-05 09:22:07 +0530

    Implement multiple catch blocks for array operations

---
+++ Java_Exception/src/exception/ArrayOperation.java
@@ -0,0 +1,16 @@
+package exception;
+
+public class ArrayOperation {
+    public static void main(String[] args) {
+        int[] arr = null;
+        int index = 2;
+        try {
+            arr = new int[]{10, 20, 30};
+            System.out.println("Value at index " + index + ": " + arr[index]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index!");
+        } catch (NullPointerException e) {
+            System.out.println("Array is not initialized!");
+        }
+    }
+}
