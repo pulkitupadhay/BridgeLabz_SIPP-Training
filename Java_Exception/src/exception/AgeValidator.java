d89cfd3d5800d282587d12759d9348b654d1069d
Pulkit Upadhyay <pulkit.upadhyay9759@gmial.com>
2025-08-05 09:20:51 +0530

    Create and use custom exception for age validation

---
+++ Java_Exception/src/exception/AgeValidator.java
@@ -0,0 +1,29 @@
+package exception;
+
+import java.util.*;
+
+class InvalidAgeException extends Exception {
+    public InvalidAgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class AgeValidator {
+    static void validateAge(int age) throws InvalidAgeException {
+        if (age < 18) {
+            throw new InvalidAgeException("Age must be 18 or above");
+        }
+        System.out.println("Access granted!");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter age: ");
+        int age = sc.nextInt();
+        try {
+            validateAge(age);
+        } catch (InvalidAgeException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
