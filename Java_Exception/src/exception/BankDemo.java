222ad592f71a1a307a25f769f003615306c4554c
Pulkit Upadhyay <pulkit.upadhyay9759@gmial.com>
2025-08-05 09:23:02 +0530

    Create bank system with custom and checked exception handling for withdrawals

---
+++ Java_Exception/src/exception/BankDemo.java
@@ -0,0 +1,39 @@
+package exception;
+
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String msg) {
+        super(msg);
+    }
+}
+
+class BankAccount {
+    double balance;
+
+    BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        } else if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        } else {
+            balance -= amount;
+            System.out.println("Withdrawal successful, new balance: " + balance);
+        }
+    }
+}
+
+public class BankDemo {
+    public static void main(String[] args) {
+        BankAccount acc = new BankAccount(5000);
+        try {
+            acc.withdraw(6000);
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
